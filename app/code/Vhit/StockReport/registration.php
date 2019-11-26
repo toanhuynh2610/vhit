96dce407f8c2cea55919c89aac6a6cc2b35a70ad
Huynh Minh Toan <toanhuynh2610@gmail.com>
2019-11-26 22:16:59 +0700

    Rewrite path for low stock threshold

---
+++ app/code/Vhit/StockReport/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: toan
+ * Date: 21/03/2019
+ * Time: 08:50
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::
+    MODULE,
+    'Vhit_StockReport',
+    __DIR__
+);
